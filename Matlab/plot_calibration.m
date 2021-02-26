5151f68c146ab337369c3246d9e0e306be33523f
rjzupkoii <rjzii@yahoo.com>
2021-02-26 15:15:29 -0500

    General beta calibration analysis plots

---
+++ Matlab/plot_calibration.m
@@ -0,0 +1,71 @@
+% plot_beta_calibration.m
+%
+% Generate a scatter plot of the EIR vs. PfPR, categorized by the popuation
+% bin that the data is in.
+clear;
+
+% Load the data
+data = csvread('data/calibration.csv', 1);
+population = transpose(unique(data(:, 3)));
+
+plot_eir_calibration(data, population);
+%plot_beta_calibration(data, population);
+
+% Plot the beta vs. PfPR values
+function [] = plot_beta_calibration(data, population)
+    labels = {};
+
+    hold on;
+    for value = population
+        pfpr = data(data(:, 3) == value, 7);
+        beta = data(data(:, 3) == value, 6);
+        scatter(beta, pfpr, 'filled');    
+        labels{end + 1} = sprintf('Population %.2d', value);
+    end
+    hold off;
+
+    % Format the x-axis
+    xlabel('Beta', 'fontsize', 24);
+    
+    format_plot(labels);
+end
+
+% Plot the EIR vs PfPR value
+function [] = plot_eir_calibration(data, population)
+    labels = {};
+
+    hold on;
+    for value = population
+        eir = log10(data(data(:, 3) == value, 6));
+        pfpr = data(data(:, 3) == value, 7);
+        beta = data(data(:, 3) == value, 6);
+        scatter(eir, pfpr, 'filled');    
+        labels{end + 1} = sprintf('Population %.2d', value);
+    end
+    hold off;
+
+    % Format the y-axis
+    xlabel('EIR', 'fontsize', 24);
+    xlim([-2 3]);
+    xticks([-4 -3 -2 -1 0 1 2 3 4]);
+    xticklabels({'0.0001', '0.001', '0.01', '0.1', '1', '10', '100', '1,000', '10,000'});
+    
+    format_plot(labels);
+end
+
+% Common function to format the plot
+function [] = format_plot(labels) 
+    % Add the PfPR ticks and labels
+    ylabel('PfPR_{2-10}', 'fontsize', 24);
+    ylim([0 100]);
+
+    % Constrain the axis
+    axis tight;
+
+    % Set title, legend, and overall font size
+    title({'Beta Calibration Data' 'EIR vs PfPR_{2-10}'}, 'fontsize', 36);
+    legend(labels, 'Location', 'northwest', 'NumColumns', 2);
+    legend('boxoff');
+    plot = gca;
+    plot.FontSize = 18;
+end
