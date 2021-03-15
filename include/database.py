837a0fc93a15ff38bb1f3d1b3c5482c8107a3260
Pakhi agarwal <pakhi_aga@yahoo.com>
2021-03-15 15:42:59 -0400

    Exception handling

--- include/database.py
+++ include/database.py
@@ -1,18 +1,47 @@
 # database.py
 #
 # This module provides a wrapper for basic database functionality.
+import sys
 import psycopg2
 
+
 def select(connectionString, sql, parameters):
+
     # Open the connection
-    connection = psycopg2.connect(connectionString)
-    cursor = connection.cursor()
+    try:
+        connection = psycopg2.connect(connectionString, connect_timeout = 5)
 
-    # Execute the query, note the rows
-    cursor.execute(sql, parameters)
-    rows = cursor.fetchall()
+    # operational error is raised when parameters passed to connect() are incorrect
+    # or if the server runs out of memory
 
-    # Clean-up and return
-    cursor.close()
-    connection.close()
-    return rows
+    except psycopg2.OperationalError as err:
+
+        print(f' Error Connecting : {err}')
+        # set connection to 'None' in case of error
+        connection = None
+
+    # connection is successful
+    if connection is not None:
+
+        cursor = connection.cursor()
+        # catch exception for invalid SQL statement
+        try:
+            # Execute the query, note the rows
+            cursor.execute(sql, parameters)
+
+    # ProgrammingError happens when there is a syntax error in the SQL statement string passed
+    # to the psycopg2 execute() method, or if a SQL statement is executed to delete a non-existent table,
+    # or an attempt is made to create a table that already exists
+
+        except psycopg2.ProgrammingError as e:
+            print(f' Syntax Error : {e}')
+            # rollback to previous transaction before starting another
+            connection.rollback()
+
+        rows = cursor.fetchall()
+
+        # Clean-up and return
+        cursor.close()
+        connection.close()
+
+        return rows
