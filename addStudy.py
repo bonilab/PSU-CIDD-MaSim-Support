164fc996f24235a378deea8fb134b13c5e6c1594
Pakhi agarwal <pakhi_aga@yahoo.com>
2021-03-18 16:25:00 -0400

    minor updates #10

--- addStudy.py
+++ addStudy.py
@@ -38,28 +38,30 @@ def add_study(configuration, stdName):
 def main():
     # Parse the parameters
     parser = argparse.ArgumentParser()
+
     parser.add_argument('-a', action='store', dest='studyname', default=None)
-    # parser.add_argument('-l', action='store', dest='projectname',default=None)
     parser.add_argument('-l', '--selectq', action='store_true', default=False)
     parser.add_argument('-c', action='store', dest='conf_file')
     args = parser.parse_args()
-    insertStudy = args.studyname
-    listStudy = args.selectq
 
     try:
         cfg = load_configuration(args.conf_file)
 
-        if insertStudy:
+        if args.studyname:
             # insert
-            studies = add_study(cfg["connection_string"], insertStudy)
+            studies = add_study(cfg["connection_string"], args.studyname)
             print("Study Id: " + str(studies))
 
-        if listStudy:
+        if args.selectq:
             # select
             rows = get_studies(cfg["connection_string"])
             if len(rows) >= 1:
+                layout = "{!s:10} {!s:14}"
+                print(layout.format("StudyName", "StudyId"))
+
                 for row in rows:
-                    print("({},{})".format(row[1], row[0]))
+                    print(layout.format(*(row[1], row[0])))
+
             else:
                 print("Table is empty, exiting")
                 sys.exit(0)
@@ -72,7 +74,8 @@ def main():
 if __name__ == "__main__":
     # Check the command line
     if len(sys.argv) < 4:
-        print("Usage: ./addStudy -c [configuration] [flag] [studyName]")
+        print("Usage-listStudy: ./addStudy -c [configuration] -l")
+        print("Usage-addStudy: ./addStudy -c [configuration] -a [studyName]")
         print("configuration - the configuration file to be loaded")
         print("flag - the operation to be performed")
         print("studyName - the name of the study to be added")
