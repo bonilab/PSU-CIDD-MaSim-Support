e887238e8c1577b57472fa6fd97c3f1782db4d2e
rjzupkoii <rjzii@yahoo.com>
2021-03-31 14:13:14 -0400

    Updating to use country code, force integer

--- bash/calibrationLib.sh
+++ bash/calibrationLib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script will queue processes on the cluster up to the configured limit. The filenames
-# will encode the values based using the name ZONE-POPUATION-ACCESS-BETA-rwa.yml although a fixed
+# will encode the values based using the name ZONE-POPUATION-ACCESS-BETA-COUNTRY.yml although a fixed
 # study id is in place as well.
 
 # Get the current job count, note the overcount due to the delay.
@@ -32,28 +32,30 @@ function run() {
   eval zone=$1
   eval population_list=$2
   eval treatment_list=$3
-  eval user=$4
+  eval country=$4
+  eval user=$5
 
   echo "Running zone, $zone"
   sed 's/#ZONE#/'"$zone"'/g' zone.asc > $zone.asc
   for population in $population_list; do
     for access in $treatment_list; do
-      for beta in `seq 0.00 0.001 0.3`; do
+      for beta in `seq 0.00 0.05 1.5`; do
         check_delay $user
 
         # Prepare the configuration file
-        sed 's/#BETA#/'"$beta"'/g' rwa-calibration.yml > $zone-$population-$access-$beta-rwa.yml
-        sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-rwa.yml  
-        sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-rwa.yml
-        sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-rwa.yml
+        sed 's/#BETA#/'"$beta"'/g' $country-calibration.yml > $zone-$population-$access-$beta-$country.yml
+        sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-$country.yml  
+        sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-$country.yml
+        sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-$country.yml
     
-        sed 's/#BETA#/'"$beta"'/g' template.job > $zone-$population-$access-$beta-rwa.pbs
-        sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-rwa.pbs
-        sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-rwa.pbs
-        sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-rwa.pbs
+        sed 's/#BETA#/'"$beta"'/g' template.job > $zone-$population-$access-$beta-$country.pbs
+        sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-$country.pbs
+        sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-$country.pbs
+        sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-$country.pbs
+        sed -i 's/#COUNTRY#/'"$country"'/g' $zone-$population-$access-$beta-$country.pbs
     
         # Queue the next item
-        qsub $zone-$population-$access-$beta-rwa.pbs
+        qsub $zone-$population-$access-$beta-$country.pbs
       done
     done
   done
@@ -71,18 +73,18 @@ function runCsv() {
     beta="$(echo "$beta"|tr -d '\r')"
 
     # Prepare the configuration file
-    sed 's/#BETA#/'"$beta"'/g' rwa-calibration.yml > $zone-$population-$access-$beta-rwa.yml
-    sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-rwa.yml  
-    sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-rwa.yml
-    sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-rwa.yml
+    sed 's/#BETA#/'"$beta"'/g' $country-calibration.yml > $zone-$population-$access-$beta-$country.yml
+    sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-$country.yml  
+    sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-$country.yml
+    sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-$country.yml
 
-    sed 's/#BETA#/'"$beta"'/g' template.job > $zone-$population-$access-$beta-rwa.pbs
-    sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-rwa.pbs
-    sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-rwa.pbs
-    sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-rwa.pbs
+    sed 's/#BETA#/'"$beta"'/g' template.job > $zone-$population-$access-$beta-$country.pbs
+    sed -i 's/#POPULATION#/'"$population"'/g' $zone-$population-$access-$beta-$country.pbs
+    sed -i 's/#ACCESS#/'"$access"'/g' $zone-$population-$access-$beta-$country.pbs
+    sed -i 's/#ZONE#/'"$zone"'/g' $zone-$population-$access-$beta-$country.pbs
 
     # Queue the next item
-    qsub $zone-$population-$access-$beta-rwa.pbs
+    qsub $zone-$population-$access-$beta-$country.pbs
 
   done < $filename
 }
